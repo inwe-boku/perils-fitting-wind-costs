29b510c0c3da7ee4d7a71e940fa126841cb504ba
ckrk <45592499+ckrk@users.noreply.github.com>
2019-05-20 18:02:39 +0200

    added revised cost model
    cleaned up version by lumbric

--- scripts/costs.py
+++ scripts/costs.py
@@ -2,35 +2,46 @@ import math
 import numpy as np
 
 
-def rinnereg(height,rated_capacity,diameter,age):
-    # Implements Eq (8) from Rinne et al 2018
-    # Output: Cost in Euro 2016
-    # Input: height in m, rated_capacity in W, diameter in m, age in "years before 2016" i think in reality its (date of availibility)-2015
+def turbine_cost(height, rated_capacity, diameter, age):
+    """Implements Eq (8) from Rinne et al 2018
+
+    Parameters
+    ----------
+    height
+        in m
+    rated_capacity
+        in W
+    diameter
+        in m
+    age
+        age in "years before 2016", maybe actually: (date of availability)-2015
+
+    Returns
+    -------
+        Cost in Euro 2016
+
+    """
     beta1 = 620
     beta2 = -1.68
     beta3 = 182
-    C  = -1005
+    C = -1005
     radius = diameter*0.5
 
     specific_power = rated_capacity/(math.pi*radius**2)
-    cost = beta1*np.log(height)+beta2*specific_power+beta3*math.sqrt(age)+C
-    #print(specific_power)
+    cost = beta1 * np.log(height) + beta2 * specific_power + beta3 * math.sqrt(age) + C
+
     return cost
 
-#rinnereg(1,1,1,1)
 
-# Testturbine 1 from Rinne et al: Year 2002 High winds V90-3.0 MW Height: 75 Price: 878 euro/kW
-# the numbers from the paper are for vintage setting age=0! and new age=1!
-#for i in range(20):
-#    print(i)
-#    print(rinnereg(75,3000000,90,i))
+if __name__ == '__main__':
+    # the numbers from the paper can be reproduced with age = 0
+    for i in range(20):
+        print(i, turbine_cost(75, 3000000, 90, i))
 
-print(rinnereg(75,3000000,90,12))
-print(rinnereg(75,3000000,90,14))
-print(rinnereg(75,3000000,90,0))
+    # Test turbine 1 (vintage) from Rinne et al:
+    # Year: 2002, High winds, V90-3.0 MW, Height: 75, Price: 878 Euro/kW
+    print("vintage:", turbine_cost(75, 3000000, 90, 0))
 
-
-
-# Testturbine 2 from Rinne et al: Year 2015 High winds V117-3.45 MW Height 125 Price:  1,448 euro/kW
-print(rinnereg(125,3450000,117,1))
-print(rinnereg(125,3450000,117,0))
+    # Test turbine 2 (new) from Rinne et al:
+    # Year: 2015, High winds, V117-3.45 MW, Height: 125, Price:  1,448 Euro/kW
+    print("new:", turbine_cost(125, 3450000, 117, 0))
