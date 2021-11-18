95b113ba08120c8d87cc122eb05699d636af6065
Johannes Schmidt <johannes.schmidt@boku.ac.at>
2021-11-18 16:52:48 +0100

    Added all files for revision

--- scripts/arising-matters.R
+++ scripts/arising-matters.R
@@ -6,93 +6,40 @@
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 
 source("functions.R")
+library(readxl)
 
-### Calculate cost curves for Rinne turbine
-### V90 3M, Radius 45 Meter, Hub height 75
+tab_filtered<-read_csv("../data/turbine-data.csv")
 
-#rotor<-45
-#hub_height<-75
-#age<-3
-#power<-3
+wrong_turbines<-tab_filtered %>% 
+  mutate(x_max=rinne_derivative_max(rotor, hub_height, age)) %>% 
+  mutate(diff=(x_max-power)/x_max) %>% 
+  arrange(diff) %>% 
+  filter(diff<0)
 
-#zond z40
-rotor<-20
-hub_height<-40
-age<-21
-power<-0.55
-
-#vergnet GEV mp-3
-#2007 mentioned here http://prdrse4all.spc.int/system/files/r_-_market_review_311007_final.pdf
-rotor<-29/2
-hub_height<-32
-age<-9
-power<-0.275
-
-#Vestas V42
-#1999 mentioned here https://www.hitwind.com/product/vestas-v42-600kw-wind-turbine/
-rotor<-39/2
-hub_height<-30
-#age<-17
-age<-17
-power<-0.5
+all_turbines <- wrong_turbines %>% 
+  bind_rows(tab_filtered %>% 
+              filter(Name=="E82/3000")) %>% 
+  mutate(min_power = c(0.27,0.72,2.2),
+    max_power = c(0.8, 1, 3.85)) 
 
 
-# all 3 turbine types found are in fact older than our approximation indicates. If we use the real age data, they are well within the plausible range.
-
-#alstom eco80
-#rotor<-90/2
-#hub_height<-80
-#age<-0
-#power<-3
+###Figure 1
+plot_feasible_infeasible_curves(all_turbines, 
+                                "figure1.png")
 
 
-theme_set(theme_bw(base_size = 16))
-
-plot_feasible_infeasible_curve(rotor,
-                               hub_height,
-                               age,
-                               "figure-1.png",
-                               power,
-                               max_power=2)
+###Figure 2
+tab_filtered %>% 
+  mutate(x_max=rinne_derivative_max(rotor, hub_height, age)) %>%
+  mutate(derivative_cost=rinne_derivative(power,rotor,hub_height,age)) %>% 
+  mutate(diff=(x_max-power)) %>% 
+  arrange(derivative_cost) %>%
+  ggplot(aes(x=derivative_cost))+
+  geom_histogram() +
+  xlab("Additional cost (€/kW)")
+ggsave("../figures/Figure2.png")
 
 
 
-### Check plausible/implausible regions for all turbines in US windturbine database
-
-### download data from US windturbine database
-f<-"../data/tempturbines.zip"
-download.file("https://eerscmap.usgs.gov/uswtdb/assets/data/uswtdbCSV.zip",f)
-unzip(f,exdir="../data")
-
-### read turbines
-turbines_download<-read_delim("../data/uswtdb_v4_1_20210721.csv",delim=",") %>% 
-  dplyr::select(t_cap,t_hh,t_rd, p_year, t_manu, t_model) %>%
-  na.omit() %>%
-  unique() %>%
-  filter(p_year<2017) %>% 
-  mutate(power=t_cap/1000,hub_height=t_hh,radius=t_rd/2,age=2016-p_year) %>%
-  mutate(age=ifelse(age<0,0,age)) %>%
-  mutate(max_power=rinne_derivative(radius,hub_height,age)) %>%
-  mutate(unlikely_region=ifelse(power<max_power,"Plausible","Implausible")) %>%
-  mutate(data_rinne = 10) %>%
-  mutate(power_density=power*10^6/(radius^2*pi)) %>% 
-  filter(!is.na(t_manu)) %>% 
-  filter(!is.na(t_model)) %>% 
-  group_by(t_manu, t_model) %>% 
-  mutate(age=max(age))
 
 
-### plot plausible and implausible turbines
-turbines_download %>%
-  filter(power_density<700) %>%
-  filter(unlikely_region=="Plausible") %>% 
-  mutate(`Hub height (m)`=hub_height) %>%
-  arrange(desc(unlikely_region)) %>%
-  mutate(Region=unlikely_region) %>%
-  ggplot(aes(x=age,y=power_density)) +
-  geom_point(aes(col=Region, size=`Hub height (m)`)) +
-  xlab("Age (Years before 2016)") +
-  ylab(bquote('Specific power (W'~m^-2~')')) +
-  scale_color_manual(values=c(colors[1],colors[2]))
-
-ggsave("../figures/figure-2.png")
--- scripts/functions.R
+++ scripts/functions.R
@@ -5,7 +5,7 @@ colors<-c("#6e9b9e", "#c72321")
 
 
 #hh..hub_height
-rinne_derivative<-function(rotor, hh, age){
+rinne_derivative_max<-function(rotor, hh, age){
 
   max_power <- (rotor^2*pi)*(620*log(hh) + 182*sqrt(age) - 1005) / (2*1.68)
   return(max_power/10^6)
@@ -16,67 +16,75 @@ rinne_total_costs<-function(power, r, hh, age){
   return(10^3*power * (610*log(hh)-1.68*power*10^6/(r^2*pi)+182*sqrt(age)-1005))
 }
 
-plot_feasible_infeasible_curve<-function(rotor, hub_height, age, filename, power = 0, max_power = 2)
-{
-
-
-  x_max<-rinne_derivative(rotor, hub_height, age)
-  y_max<-rinne_total_costs(x_max, rotor, hub_height ,age)/10^6
-
-  print(paste0("Power density Turbine:",power*1E6/(rotor^2*pi)))
-  print(paste0("Power density at max:",x_max*1E6/(rotor^2*pi)))
-  print(paste0("xmax:",x_max))
+rinne_derivative<-function(power, r, hh, age){
   
+  power<-10^3*power
+  return(610*log(hh)-1.68*2*power*10^3/(r^2*pi)+182*sqrt(age)-1005)
+  
+}
 
-  power_range_feasible<-seq(0,x_max,0.01)
+
+
+get_feasible_curve<-function(data){
+  power_range_feasible<-seq(data$min_power,data$x_max,0.01)
   feasible_curve<-tibble(power_range=power_range_feasible,
-                         total_costs=rinne_total_costs(power_range_feasible,
-                                                       rotor,
-                                                       hub_height,
-                                                       age)/10^6,
-                         Region="Plausible")
+                          total_costs=rinne_total_costs(power_range_feasible,
+                                                        data$rotor,
+                                                        data$hub_height,
+                                                        data$age)/10^6)
+  return(feasible_curve)
+  
+}
 
-  power_range_infeasible<-seq(x_max,max_power,0.01)
+
+get_infeasible_curve<-function(data){
+  
+  power_range_infeasible<-seq(data$x_max,data$max_power,0.01)
   infeasible_curve<-tibble(power_range=power_range_infeasible,
-                           total_costs=rinne_total_costs(power_range_infeasible,
-                                                         rotor,
-                                                         hub_height,
-                                                         age)/10^6,
-                           Region="Implausible"
-  )
+                             total_costs=rinne_total_costs(power_range_infeasible,
+                                                        data$rotor,
+                                                        data$hub_height,
+                                                        data$age)/10^6) 
+  return(infeasible_curve)
+}
 
-  if(power != 0){
-
-    cost_max_power=rinne_total_costs(power,
-                                     rotor,
-                                     hub_height,
-                                     age)/10^6
-
-    figure<-bind_rows(feasible_curve, infeasible_curve) %>%
-      mutate(`Turbine`="Vestas V42") %>%
-      ggplot(aes(x=power_range,y=total_costs)) +
-      geom_line(aes(col=Region),size=2) +
-      xlab("Rated power (MW)") + ylab("Total costs of Turbine \n(Million Euro)") +
-      geom_point(aes(x=x_max,y=y_max),size=2) +
-      scale_color_manual(values=c(colors[1],colors[2])) +
-      geom_point(aes(x=power, y=cost_max_power, shape=`Turbine`), size=6, stroke=3)+
-      scale_shape_manual(values=c(4))
-
-  } else{
-
-
-
-
-    figure<-bind_rows(feasible_curve, infeasible_curve) %>%
-      ggplot(aes(x=power_range,y=total_costs)) +
-      geom_line(aes(col=Region),size=2) +
-      xlab("Power (MW)") + ylab("Total costs of Turbine (Million Euro)") +
-      geom_point(aes(x=x_max,y=y_max),size=2) +
-
-      scale_color_manual(values=c(colors[1],colors[2]))
-  }
-
-  plot(figure)
+plot_feasible_infeasible_curves<-function(turbine_data, filename)
+{
+  
+  
+  turbine_data<-turbine_data %>% 
+    mutate(x_max=rinne_derivative_max(rotor, hub_height, age)) %>% 
+    mutate(y_max=rinne_total_costs(x_max, rotor, hub_height, age)/10^6)
+  
+  p<-turbine_data %>% 
+    group_by(Name) %>% 
+    nest() %>% 
+    mutate(feasible_range=map(data, get_feasible_curve)) %>%
+    unnest(data) %>% 
+    unnest(feasible_range) %>% 
+    mutate(Region="Feasible range") %>% 
+    bind_rows(turbine_data %>% 
+                group_by(Name) %>% 
+                nest() %>% 
+                mutate(infeasible_range=map(data, get_infeasible_curve)) %>%
+                unnest(data) %>% 
+                unnest(infeasible_range) %>% 
+                mutate(Region="Infeasible range")) %>% 
+    mutate(Turbine=Name) %>% 
+    ggplot(aes(x=power_range,y=total_costs)) + 
+    geom_line(aes(col=Region) ,size=2) +
+    facet_wrap(.~Turbine,scales="free") +
+    scale_color_manual(values=c(colors[1],colors[2])) +
+    geom_point(aes(x=power,y=rinne_total_costs(power, rotor, hub_height, age)/10^6,shape=Turbine),size=4) +
+    xlab("Turbine power (MW)") +
+    ylab("Total turbine cost \n as projected by model (M€)")
+    
+  plot(p)
+  
   ggsave(paste0("../figures/", filename))
+  
+    
 
 }
+  
+  
